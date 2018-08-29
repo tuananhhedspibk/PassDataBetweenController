8255c0b958d3e1d5838525bf2cf25b635bbda7f1
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2018-08-29 14:18:05 +0700

    Initial Commit

---
+++ PassDataBetweenViewController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PassDataBetweenViewController
+//
+//  Created by mylaptop on 8/29/18.
+//  Copyright © 2018 mylaptop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
